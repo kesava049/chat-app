211a5686d2fb52b91c429ba0919dc374b4f8c50e
kesava049 <kalepallikesavulareddy@gmail.com>
2024-07-01 19:21:49 +0530

    Socket url updated

--- frontend/src/context/SocketContext.jsx
+++ frontend/src/context/SocketContext.jsx
@@ -15,7 +15,7 @@ export const SocketContextProvider = ({ children }) => {
 
 	useEffect(() => {
 		if (authUser) {
-			const socket = io("https://localhost:5002", {
+			const socket = io("https://chat-app-kfx5.onrender.com/", {
 				query: {
 					userId: authUser._id,
 				},
